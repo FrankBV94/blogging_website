51ff4d9419b5bd8c8320a501c9ce19a90aa40b39
FrankBV94 <frankbotelle@gmail.com>
2023-11-24 03:36:55 -0500

    NavBar

--- blog_frontend/src/App.jsx
+++ blog_frontend/src/App.jsx
@@ -1,6 +1,10 @@
+import Navbar from './components/Navbar'
+
 const App = () => {
   return (
-    <h1>MERN Blogging website by modern web</h1>
+    <div className=''>
+      <Navbar />
+    </div>
   )
 }
 
