66f57159b1fb39d2dd85a7a74a9dc9c7a0b8747d
FrankBV94 <frankbotelle@gmail.com>
2023-11-25 02:55:19 -0500

    Routes

--- blog_frontend/src/App.jsx
+++ blog_frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import useTheme from './hooks/useTheme'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 const App = () => {
+  const { darkMode } = useTheme()
+
   return (
-    <div className=''>
-      <Navbar />
-    </div>
+    <Routes>
+      <Route path='/' element={<Navbar />} >
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Route >
+    </Routes>
   )
 }
 
